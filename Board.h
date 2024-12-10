9abf6696f921a81e2a43954f8ea4a4119fac33e9
Tomek-0 <272135@student.pwr.edu.pl>
2024-12-10 19:13:22 +0100

    Fixed erros with Board

--- Board.h
+++ Board.h
@@ -1,43 +1,38 @@
-#ifndef Board_H
-#define Board_H
+#ifndef BOARD_H
+#define BOARD_H
 
 #include <iostream>
+#include <vector>
 
-class Board{
-    private:
+class Board {
+private:
     int size;
     int first_row_pins;
-    int middle = WIDTH/2;
-    int left_middle=first_row_pins-1;
-    int right_middle = WIDTH - left_middle;
+    int HEIGHT;
+    int WIDTH;
+    int middle;
+    int left_middle;
+    int right_middle;
+    std::vector<std::vector<char>> Fileds;
 
-    int HEIGHT = 2*size - 1 + 2*first_row_pins;
-    int WIDTH = ((size + 2*first_row_pins)-1)*2;
-
-    //TODO: Fix error - change to vector
-    char Fileds[HEIGHT][WIDTH+1]; 
     
+    static constexpr char Alphabet[27] = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'};
 
-    //Delete in future
-    char Alphabet[27]={'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'};
-
-    public:
-    //Constructor
+public:
     Board(int board_size);
 
-
+    void fill_empty_space();
     void fill_top_triangle(char x);
-    void fill_bottom_trinalge(char x);
+    void fill_bottom_triangle(char x);
     void fill_top_left(char x);
     void fill_top_right(char x);
     void fill_bot_left(char x);
     void fill_bot_right(char x);
     void setup_board(int num_players);
     void move_checker();
-    void check_legality();
+    bool check_legality();
     void showBoard();
-
+    void fill_hexagon();
 };
 
-
-#endif 
+#endif
